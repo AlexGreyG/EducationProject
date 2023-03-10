357628a5fca55b38dbe7834c813b5f615bb53a97
AlexGreyG <alexsgorsky@gmail.com>
2023-03-10 10:37:19 +0300

    update

--- app/src/main/java/com/example/educationproject/MainActivity.kt
+++ app/src/main/java/com/example/educationproject/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.educationproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadData {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun loadData(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Sos me")
+        }
     }
 }
